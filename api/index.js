51e91d063df30e67d117422c76533f26a9052764
Maksim Gachevskiy <maksim.gachevskiy@zensoft.io>
2019-05-04 12:28:33 +0600

    Init api

---
+++ api/index.js
@@ -0,0 +1,23 @@
+const express = require("express");
+const cors = require("cors");
+const mongoose = require("mongoose");
+const config = require("./config");
+
+const app = express();
+require("express-ws")(app);
+const db = mongoose.connection;
+
+const port = process.env.PORT||8000;
+
+app.use(cors());
+app.use(express.json());
+mongoose.connect(config.db);
+
+
+
+db.once("open", () => {
+
+    app.listen(port, () => {
+        console.log(`Server started on ${port} port!`);
+    });
+});
